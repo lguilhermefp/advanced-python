4818fe0563b3d68b7c57513afb6d847c265750fe
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-09-23 22:27:36 -0300

    challenge 1

---
+++ challenges/c1-matrix.py
@@ -0,0 +1,36 @@
+import numpy as np
+import math
+
+def main():
+	vector = defineVector()
+	print(vector)
+
+
+
+def defineVector():
+	newVector = np.array([0.0] * 10)
+	for index, value in enumerate(newVector):
+		newVector[index] = associateValueForIndex(index)
+	return newVector
+
+
+
+def associateValueForIndex(x):
+		if x % 2 == 0:
+			return defineValuesForEvenIndex(x)
+		return defineValuesForOddIndex(x)
+
+
+
+def defineValuesForEvenIndex(x):
+	return pow(3, x) + 7*math.factorial(x)
+
+
+
+def defineValuesForOddIndex(x):
+	return pow(2, x) + 4*math.log(x)
+
+
+
+if __name__ == "__main__":
+	main()
